ed9b66c9d8170bcc0cabbaa9373d358a53330b90
Adam Cardenas <cevaris@gmail.com>
2015-10-18 15:05:33 -0600

    abstracted LinearProbeHash out into hash/collision funcs

--- src/main/scala/com/cevaris/hashes/QuadraticProbeHash.scala
+++ src/main/scala/com/cevaris/hashes/QuadraticProbeHash.scala
@@ -11,11 +11,10 @@ class QuadraticProbeHash[A](implicit m: ClassTag[A]) extends Hashed[A] {
   var table: Array[KeyValue[A]] = empty()
 
   override def get(key: Int): Option[A] =
-    getLinearProbe(key, key % defaultSize)
+    getLinearProbe(key, 0)
 
   override def set(key: Int, value: A): A = {
-    val initialIndex: Int = key % defaultSize
-    table = setLinearProbe(key, initialIndex, value)
+    table = setLinearProbe(key, 0, value)
     value
   }
 
@@ -31,7 +30,19 @@ class QuadraticProbeHash[A](implicit m: ClassTag[A]) extends Hashed[A] {
   private def empty(): Array[KeyValue[A]] =
     new Array(defaultSize)
 
-  private def getLinearProbe(key: Int, currentIndex: Int): Option[A] = {
+  private def collisionFunction(attempt: Int): Int =
+    attempt + 1
+
+  private def hashFunction(key: Int): Int =
+    key
+
+  private def hash(key: Int, attempt: Int, currSize: Int) = {
+    (hashFunction(key) + collisionFunction(attempt)) % currSize
+  }
+
+
+  private def getLinearProbe(key: Int, attempt: Int): Option[A] = {
+    val currentIndex = hash(key, attempt, defaultSize)
     table.lift(currentIndex) match {
       case None | Some(null) => None
       case Some(currKeyValue) =>
@@ -39,19 +50,20 @@ class QuadraticProbeHash[A](implicit m: ClassTag[A]) extends Hashed[A] {
           Some(currKeyValue.value)
         } else {
           log.warning(s"Collision with $currKeyValue at $currentIndex when looking for $key")
-          getLinearProbe(key, currentIndex + 1)
+          getLinearProbe(key, attempt + 1)
         }
     }
 
   }
 
-  private def setLinearProbe(key: Int, currentIndex: Int, value: A): Array[KeyValue[A]] = {
+  private def setLinearProbe(key: Int, attempt: Int, value: A): Array[KeyValue[A]] = {
+    val currentIndex = hash(key, attempt, defaultSize)
     table.lift(currentIndex) match {
       case None | Some(null) =>
         table.updated(currentIndex, KeyValue(key, value))
       case Some(currKeyValue) =>
         log.warning(s"Collision with $currKeyValue at $currentIndex")
-        setLinearProbe(key, currentIndex + 1, value)
+        setLinearProbe(key, attempt + 1, value)
     }
   }
 
